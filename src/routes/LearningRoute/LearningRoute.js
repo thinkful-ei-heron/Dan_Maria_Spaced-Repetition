573ea66855daa6029ddcacfae34096a05ed391f2
Maria Danielson <mal3905@gmail.com>
2019-12-30 16:41:48 -0500

    in prog

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -8,8 +8,11 @@ class LearningRoute extends Component {
   state = {
     head: '',
     total: '',
-    
-
+    guess: '',
+    answer: '',
+    correctcount: '',
+    incorrectcount: '',
+    original: ''
 
   }
   componentDidMount() {
@@ -27,18 +30,77 @@ class LearningRoute extends Component {
       : res.json())
       .then( data => {
         this.setState({
-          head: data
+          head: data.next, 
+          total: data.total,
+          correctcount: data.correctcount,
+          incorrectcount: data.incorrectcount,
+          original: data.original,
         })
-
     })
   }
-  render() {
+  handleSubmit=(e) => {
+    e.preventDefault();
+    fetch(`${config.API_ENDPOINT}/language/guess`, {
+      method:'POST',
+      headers: {
+        Authorization: `Bearer ${TokenService.getAuthToken()}`,
+        "content-type": "application/json"
+      },
+      body: JSON.stringify({guess: this.state.guess})  
+    })
+    .then( res => 
+      (!res.ok) 
+      ? res.json()
+      .then( e => Promise.reject(e))
+      :res.json())
+      .then( data => {
+        this.setState({
+          answer: data.answer, 
+          head: data.next,
+          total: data.total,
+          correctcount: data.correctcount,
+          incorrectcount: data.incorrectcount,
+          original: this.state.head
+        })
+      })
+  }
+  handleNextWord = () => {
+    this.setState({answer: '', guess: ''})
+  }
+  handleguess = () => {
+    const guess = e.target.value; 
+    let string = guess.toLowerCase();
+    this.setState({ guess: string})
+  }
+
+
+  renderFeedback() {
+    if(this.state.isCorrect === true) {
+      return (
+        <div className="iscorrectFeedback">
+          <h1>You were correct! :D</h1>
+          <h2> The correct translation for {this.state.original} was {this.state.answer} and you chose {this.state.guess}!</h2>
+        </div>
+      )
+    }
+    else if(this.state.isCorrect === false){
+      return(
+        <div className="iscorrectFeedback">
+        <h1>Good try, but not quite right :(</h1>
+        <h2> The correct translation for {this.state.original} was {this.state.answer} and you chose {this.state.guess}!</h2>
+      </div>
+      )
+    }
     return (
       <section className="learning">
         <h1 className='learning-score'>
           {`Your total score is:${this.state.total}`}
         </h1>
       </section>
+
+      <div className='check-guess'>
+        {this.state.guess}
+      </div>
     );
   }
 }
