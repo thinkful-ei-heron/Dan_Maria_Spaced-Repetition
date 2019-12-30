0b60071a6cf29e5818a6bd9ba75af81096a97710
Maria Danielson <mal3905@gmail.com>
2019-12-30 15:31:05 -0500

    learning route

--- src/routes/LearningRoute/LearningRoute.js
+++ src/routes/LearningRoute/LearningRoute.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react'
+import config from '../../config'
+import TokenService from '../../services/token-service'
+
 
 class LearningRoute extends Component {
+
+  state = {
+    head: '',
+    total: '',
+    
+
+
+  }
+  componentDidMount() {
+    fetch(`${config.API_ENDPOINT}/language/head`,{
+      method:'GET',
+      headers:{
+        Authorization: `Bearer ${TokenService.getAuthToken()}`
+      }
+    })
+    
+    .then( res => 
+      (!res.ok)
+      ? res.json()
+      .then( e => Promise.reject(e))
+      : res.json())
+      .then( data => {
+        this.setState({
+          head: data
+        })
+
+    })
+  }
   render() {
     return (
-      <section>
-        implement and style me
+      <section className="learning">
+        <h1 className='learning-score'>
+          {`Your total score is:${this.state.total}`}
+        </h1>
       </section>
     );
   }
